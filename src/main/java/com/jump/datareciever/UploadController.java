540ba9c2bc26136d32d37ccf2391ee249f46675b
li2niu <chuanyi@protonmail.com>
2021-03-19 03:33:03 +0800

    modify datastructure

--- src/main/java/com/jump/datareciever/UploadController.java
+++ src/main/java/com/jump/datareciever/UploadController.java
@@ -1,10 +1,7 @@
 package com.jump.datareciever;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,22 +14,27 @@ import java.util.List;
 @RestController
 public class UploadController {
 
-    private List<Double> xDataList = new ArrayList<>();
-    private List<Double> yDataList = new ArrayList<>();
-    private List<Double> zDataList = new ArrayList<>();
+    private List<List<Double>> xDataList = new ArrayList<>();
+    private List<List<Double>> yDataList = new ArrayList<>();
+    private List<List<Double>> zDataList = new ArrayList<>();
     private int batch = 0;
 
     @PostMapping("/upload")
     public int uploadData(@RequestBody AccelData data){
         ++batch;
         System.out.printf("------Batch %d Data receiving------%n", batch);
-        xDataList.addAll(data.getX());
-        yDataList.addAll(data.getY());
-        zDataList.addAll(data.getZ());
+        xDataList.add(data.getX());
+        yDataList.add(data.getY());
+        zDataList.add(data.getZ());
         System.out.println("x axis :"+xDataList);
         System.out.println("y axis :"+yDataList);
         System.out.println("z axis :"+zDataList);
         System.out.printf("------Batch %d Data received------%n", batch);
         return 0;
+    }
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "Do you run today?";
     }
 }
