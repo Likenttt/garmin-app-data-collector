cb0c890faefb40ca7aad4824f764988f6b37d312
li2niu <chuanyi@protonmail.com>
2021-03-20 17:52:04 +0800

    start sh

--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
-kill $(ps -ef | grep "jump-data-reciever-0.0.1-SNAPSHOT.jar" | grep -v grep | awk '{print $2}')
+rm -rf target
+git pull
 mvn package
+kill `ps -ef | grep jump-data-reciever-0.0.1-SNAPSHOT.jar | grep -v grep | awk '{print $2}'`
 nohup java -jar target/jump-data-reciever-0.0.1-SNAPSHOT.jar &!
