b9db90447750c0c9b73c0804b0d4ee3d51605e31
li2niu <chuanyi@protonmail.com>
2021-03-27 19:21:04 +0800

    use redis to store accel data

---
+++ src/main/java/com/jump/datareciever/entity/DataCollection.java
@@ -0,0 +1,22 @@
+package com.jump.datareciever.entity;
+
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author: chuanyi@88.com
+ * @description:
+ * @date: 2021/3/27 18:41
+ */
+@Data
+public class DataCollection implements Serializable {
+    private static final long serialVersionUID = -4848531928419020951L;
+    private String mark;
+    private int batchCount;
+    private List<List<Double>> x = new ArrayList<>();
+    private List<List<Double>> y = new ArrayList<>();
+    private List<List<Double>> z = new ArrayList<>();
+}
