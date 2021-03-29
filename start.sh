c1e24d831be13e7060842bae7022c716aad9821b
li2niu <chuanyi@protonmail.com>
2021-03-29 23:20:12 +0800

    fix start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 rm -rf target
 git pull
-mv ~/application.properties src/main/resources/
+cp ~/application.properties src/main/resources/
 mvn package -Dtest.skip=true
 rm nohup.out
 kill `ps -ef | grep jump-data-reciever-0.0.1-SNAPSHOT.jar | grep -v grep | awk '{print $2}'`
